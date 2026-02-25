43fcfdf7418595c7899f6f97d0acc1f15cd6b84a
Michaelgwei86 <michaelgwei86@gmail.com>
2026-02-25 01:38:03 +0000

    updating git config files

--- JavaLoginApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaLoginApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package hsbc.payment.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- JavaLoginApp/src/test/java/com/example/demo/AuthFlowTest.java
+++ JavaLoginApp/src/test/java/com/example/demo/AuthFlowTest.java
@@ -1,4 +1,4 @@
-package hsbc.payment.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
