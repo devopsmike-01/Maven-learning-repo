eb7d18ef38ea27f529049d096dc34e94aa90d05f
Michaelgwei86 <michaelgwei86@gmail.com>
2026-02-25 01:30:34 +0000

    updating git config files

--- hsbc-payment-gateway/src/main/java/com/example/demo/security/SecurityConfig.java
+++ hsbc-payment-gateway/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.security;
+package hsbc.payment.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
